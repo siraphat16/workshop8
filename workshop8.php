4dca457def67e1e04b720b4aa814f846e8e3dfcf
siraphat16 <109897886+siraphat16@users.noreply.github.com>
2022-10-09 15:23:15 +0700

    Add files via upload

---
+++ workshop8.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+<form action="insert.php" method="post">
+	username: 	<input type="text" name="username"><br><br>
+	name: 		<input type="text" name="name"><br><br>
+	address:	<textarea name="address" rows="3" cols="40"></textarea><br><br>
+	password: 	<input type="number" name="password"><br><br>
+	mobile:		<input type="text"  name="mobile"><br><br>
+	email:		<input type="text"  name="email">
+	<br><br>
+	<input type="submit" value="submit">
+</form>
+</body>
+</html>
